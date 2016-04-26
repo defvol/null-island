ccf498a3d3a9f76d57c46b8934428879b05547e9
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-04-26 08:29:54 -0400

    Reads from the pipe stream to generate many null islands

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+var through = require('through2');
 
 module.exports.nullIsland = function () {
   var g = '{"type":"Point","coordinates":[0,0]}';
   var f = '[{"type":"Feature","properties":{},"geometry":' + g + '}]';
-  return '{"type":"FeatureCollection","features":' + f + '}';
+  return '{"type":"FeatureCollection","features":' + f + '}\n';
+}
+
+module.exports.throughNullIsland = function() {
+  var nils = this.nullIsland;
+  return through(function (chunk, _, next) {
+    this.push(nils());
+    next();
+  });
 }
--- test.js
+++ test.js
@@ -1,10 +1,26 @@
-var test = require('tape').test;
 var nils = require('./index');
+var Readable = require('stream').Readable;
+var test = require('tape').test;
+var through = require('through2');
 
 test('The Republic of Null Island', function (t) {
-  t.plan(3);
+  t.plan(4);
   var geojson = JSON.parse(nils.nullIsland());
   t.equal(geojson.features.length, 1, 'has only one Feature');
   t.equal(geojson.features[0].geometry.type, 'Point', 'which is a Point');
   t.deepEqual(geojson.features[0].geometry.coordinates, [0, 0], '0°N 0°E');
+
+  var want = nils.nullIsland().concat(nils.nullIsland());
+  var rs = Readable();
+  var buffer = '';
+  var ts = through(function (chunk, _, next) {
+    buffer += chunk;
+    next();
+  }, function (done) {
+    t.equal(buffer.toString(), want, 'Found 2 Null Islands');
+  });
+  rs.pipe(nils.throughNullIsland()).pipe(ts);
+  rs.push('foo\n');
+  rs.push('bar\n');
+  rs.push(null);
 });
